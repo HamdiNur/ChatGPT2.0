799e2349e9d690342f769e34e9d6504f6dba63bc
HamdiNur <hamdoonur26@gmail.com>
2024-08-12 10:41:19 +0300

    finished this one

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChatBody = () => {
     const aistyle="bg-white bg-opacity-40 backdrop-blur-lg dropshadow-md mr-auto"
   return (
-   <div className="flex flex-col gap4">
+   <div className="flex flex-col gap-4">
     {/* {Client messagel} */}
     <div className='border-[#999999] break-words border-2 rounded-xl self-end px-3  py-3 max-w-[80%]'>
         <pre className='whitespaces-pre-wrap'>
